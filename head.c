4d415180e4e3f7a7741487c7d86915203e8a4807
tasty-dumpling <143873319+tasty-dumpling@users.noreply.github.com>
2023-09-02 20:02:47 +0900

    added Q11

--- head.c
+++ head.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// 사용자에게 정수를 입력받는 함수
 int get_num(void) {
 	int number;
 	printf("정수를 입력하세요.");
@@ -13,6 +14,7 @@ int get_num(void) {
 	return number;
 }
 
+// 수 배열의 평균값을 구하는 함수
 float get_average(int array[], int size) {
 	float sum = 0;
 	for (int i = 0; i < size; i++) {
@@ -21,7 +23,8 @@ float get_average(int array[], int size) {
 	return sum / size;
 }
 
-double get_dev(int array[], int size, float average) {
+// 수 배열의 표준 편차를 구하는 함수
+double get_stddev(int array[], int size, float average) {
 	double temp = 0.0;
 	for (int i = 0; i < size; i++) {
 		temp = temp + pow(array[i] - average, 2);
@@ -29,4 +32,21 @@ double get_dev(int array[], int size, float average) {
 	temp = temp / size;
 	temp = sqrt(temp);
 	return temp;
+}
+
+// 벡터 A와 B의 합을 배열C로 저장
+void add_vector(double A[], double B[], double C[], int size) {
+	for (int i = 0; i < size; i++) {
+		C[i] = A[i] + B[i];
+	}
+}
+
+// 벡터 A와 B의 내적을 구하는 함수
+double dot_prod_vector(double A[], double B[], int size) {
+	double dot_prod = 0.0;
+
+	for (int i = 0; i < size; i++) {
+		dot_prod += A[i] * B[i];
+	}
+	return dot_prod;
 }
