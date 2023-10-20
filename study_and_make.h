c954f22abed5678d7ff7097d6a1503d0a8ebc905
tasty-dumpling <143873319+tasty-dumpling@users.noreply.github.com>
2023-10-20 18:10:06 +0900

    Create study_and_make.h

---
+++ study_and_make.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159265358979323846
+
+int get_integer(char a[]);
+double get_floating(char a[]);
+
+void CHAP2_Q1(void);
+void CHAP2_Q2(void);
+void CHAP2_Q3(void);
+void CHAP2_Q4(void);
+void CHAP2_Q5(void);
+void make_moon_with_lego(void);
+void CHAP3_Q1(void);
